8288b6910e50a622bfe9d1654c949467d3f6a25d
AVKostin <alexander.kostin.ua@gmail.com>
2022-04-28 18:29:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import Painting from './components/Painting';
+import paintings from './paintings.json';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+function Painting() {
+  return (
+    <div>
+      <img src="" alt="" width="480" />
+      <h2></h2>
+      <p>
+        Автор: <a href=""></a>
+      </p>
+      <p>Цена: кредитов</p>
+      <p>Доступность: заканчивается или есть в наличии}</p>
+      <button type="button">Добавить в корзину</button>
+    </div>
+  );
+}
+
+ReactDOM.render(<Painting />, document.querySelector('#root'));
+
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root'),
+// );
